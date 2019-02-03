c8ac00e1fad1033854bf2e49b381930893a357c8
Julian Hoyos <jhoyos135@gmail.com>
2019-02-02 20:59:31 -0500

    fixed bug with pagination

--- google.js
+++ google.js
@@ -13,19 +13,19 @@ let google = (lat, lon, name, id, website) => {
           <div class="modal-content">
             <h4>${name}</h4>
     
-            //google url
-            https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyDn7dkJJ7QZJ3FZ7AhvoIjTvN9OceO8VuA
     
           </div>
         </div>
 
+        <!-- Website modal -->
         <div id="website-${id}" class="modal" style="height:90%; width: 90%;">
           <div class="modal-content" style="height:90%; width: 90%; margin: 0 auto;">
-            <h4>Website</h4>
+            <h4> ${name} - Website</h4>
     
             <iframe width="100%" height="100%" frameborder="0" allowtransparency="true" src="${website}"></iframe>
     
           </div>
         </div>
         `;
+
 };
